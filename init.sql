41408999ecc2eb74b2ed7ac26fc9d82165c03052
Boris Martynov <bamartynov@edu.hse.ru>
2026-02-17 14:37:01 +0300

    [DB] fixed init.sql, upsert.go, models.go
    init sql now works with JiraID not increment
    upsert now has atomic operatons
    modles added correct handle for null cases and added json implement for api

--- init.sql
+++ init.sql
@@ -1,63 +1,62 @@
-DROP USER IF EXISTS pguser;
-DROP USER IF EXISTS replicator;
---оставляем для коректного использования в начале работы, для k8s будет не нужно
+DROP ROLE IF EXISTS pguser;
+DROP ROLE IF EXISTS replicator;
 
-CREATE USER pguser WITH PASSWORD 'pgpwd';
-CREATE USER replicator WITH PASSWORD 'postgres' REPLICATION LOGIN;
---два пользователя для работы бд и для потоковой репликации, user postgres автоматически создается докером
+CREATE ROLE pguser WITH LOGIN PASSWORD 'pgpwd';
+CREATE ROLE replicator WITH LOGIN PASSWORD 'postgres' REPLICATION;
 
 CREATE TABLE project (
-    id SERIAL PRIMARY KEY,
-    key VARCHAR(50) UNIQUE NOT NULL, --идентификатор из JIRA
+    jira_id BIGINT PRIMARY KEY,
+    key VARCHAR(50) UNIQUE NOT NULL,
     name VARCHAR(255) NOT NULL,
     url TEXT
 );
 
 CREATE TABLE author (
-    id SERIAL PRIMARY KEY,
+    jira_id BIGINT PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
     email VARCHAR(255)
 );
 
 CREATE TABLE issues (
-    id SERIAL PRIMARY KEY,
-    project_id INTEGER NOT NULL,
+    jira_id BIGINT PRIMARY KEY,
+    project_id BIGINT NOT NULL,
     key VARCHAR(50) NOT NULL,
     summary TEXT,
     status VARCHAR(100),
     priority VARCHAR(50),
-    created_time TIMESTAMP,
+    created_time TIMESTAMP NOT NULL,
     updated_time TIMESTAMP,
-    closed_time TIMESTAMP,
+    closed_time TIMESTAMP NULL,
     time_spent INTEGER,
-    creator_id INTEGER,
-    assignee_id INTEGER,
-    FOREIGN KEY (project_id) REFERENCES project(id) ON DELETE CASCADE, --если удалили проект удаляем все задачи
-    FOREIGN KEY (creator_id) REFERENCES author(id),
-    FOREIGN KEY (assignee_id) REFERENCES author(id),
-    CONSTRAINT unique_issue_key UNIQUE(project_id, key) 
+    creator_id BIGINT,
+    assignee_id BIGINT,
+
+    FOREIGN KEY (project_id) REFERENCES project(jira_id) ON DELETE CASCADE,
+    FOREIGN KEY (creator_id) REFERENCES author(jira_id),
+    FOREIGN KEY (assignee_id) REFERENCES author(jira_id),
+
+    CONSTRAINT unique_issue_key UNIQUE(project_id, key)
 );
 
 CREATE TABLE status_change (
-    id SERIAL PRIMARY KEY,
-    issue_id INTEGER NOT NULL,
+    id BIGSERIAL PRIMARY KEY,
+    issue_id BIGINT NOT NULL,
     old_status VARCHAR(100),
     new_status VARCHAR(100),
-    change_time TIMESTAMP,
-    FOREIGN KEY (issue_id) REFERENCES issues(id) ON DELETE CASCADE
+    change_time TIMESTAMP NOT NULL,
+
+    FOREIGN KEY (issue_id) REFERENCES issues(jira_id) ON DELETE CASCADE
 );
 
-CREATE INDEX idx_project_key ON project(key);
-
-CREATE INDEX idx_author_username ON author(username);
-
-CREATE INDEX idx_issues_project_id ON issues(project_id);
-CREATE INDEX idx_issues_status ON issues(status);
-CREATE INDEX idx_issues_creation_time ON issues(created_time);
-CREATE INDEX idx_issues_deletion_time ON issues(closed_time);
-
-CREATE INDEX idx_statuschange_issue_id ON status_change(issue_id);
-CREATE INDEX idx_statuschange_change_time ON status_change(change_time);
+CREATE INDEX IF NOT EXISTS idx_project_key ON project(key);
+CREATE INDEX IF NOT EXISTS idx_author_username ON author(username);
+CREATE INDEX IF NOT EXISTS idx_issues_project_id ON issues(project_id);
+CREATE INDEX IF NOT EXISTS idx_issues_status ON issues(status);
+CREATE INDEX IF NOT EXISTS idx_issues_priority ON issues(priority);
+CREATE INDEX IF NOT EXISTS idx_issues_created_time ON issues(created_time);
+CREATE INDEX IF NOT EXISTS idx_issues_closed_time ON issues(closed_time);
+CREATE INDEX IF NOT EXISTS idx_statuschange_issue_id ON status_change(issue_id);
+CREATE INDEX IF NOT EXISTS idx_statuschange_change_time ON status_change(change_time);
 
 GRANT USAGE ON SCHEMA public TO pguser;
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO pguser;
@@ -67,10 +66,16 @@ GRANT CONNECT ON DATABASE testdb TO replicator;
 GRANT USAGE ON SCHEMA public TO replicator;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO replicator;
 
---Logical replecation test version for CQRS and Logical replication in future
-ALTER SYSTEM SET wal_level = logical;
-ALTER SYSTEM SET max_replication_slots = 10;
-ALTER SYSTEM SET max_logical_replication_workers = 10;
-ALTER SYSTEM SET max_worker_processes = 10;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO pguser;
 
-ALTER USER replicator REPLICATION LOGIN;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+GRANT USAGE, SELECT ON SEQUENCES TO pguser;
+
+--Logical replecation test version for CQRS and Logical replication in future
+--ALTER SYSTEM SET wal_level = logical;
+--ALTER SYSTEM SET max_replication_slots = 10;
+--ALTER SYSTEM SET max_logical_replication_workers = 10;
+--ALTER SYSTEM SET max_worker_processes = 10;
+--TODO
+--ALTER USER replicator REPLICATION LOGIN;
